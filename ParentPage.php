5776d15b638faf5dbf4bd5c307565297cc2aa753
Umherirrender <umherirrender_de.wp@web.de>
2021-12-07 12:29:57 +0100

    Emit deprecation warning from php entry point
    Added an old, but not to-outdated required version to set a minimum
    requirement
    
    Change-Id: Id2acfea3552179f68d3f5c48159daa2722cc647f

--- ParentPage.php
+++ ParentPage.php
@@ -1,5 +1,10 @@
 <?php
 
+wfWarn(
+	'Deprecated PHP entry point used for ParentPage extension. Please use wfLoadExtension instead, ' .
+	'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+);
+
 wfLoadExtension( "ParentPage" );
 
 //register new field for parent page, use BlueSpiceSMWConnector Extension
